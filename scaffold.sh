65ce07e9408ec4d771d6783773e468114ffd232e
bnechyporenko <bogdannechiporenko@gmail.com>
2022-01-03 15:13:59 +0100

    wip

--- scaffold.sh
+++ scaffold.sh
@@ -1,6 +1,9 @@
 cd golang
 rm -rf *
-operator-sdk init --domain digital.ai --repo github.com/xebialabs/xl-deploy-kubernetes-operator
+operator-sdk init --domain digital.ai --repo github.com/xebialabs/xl-deploy-kubernetes-operator --project-version=3
 operator-sdk create api --group xld --version v1alpha1 --kind DigitalaiDeploy --resource --controller
+operator-sdk olm install
 
-
+#curl -L https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.19.1/install.sh -o install.sh
+#chmod +x install.sh
+#./install.sh v0.19.1
