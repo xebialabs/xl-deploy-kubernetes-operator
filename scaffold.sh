3e3f87f377a17101e5f3ca604ff47290683d27bf
bnechyporenko <bogdannechiporenko@gmail.com>
2022-01-04 12:39:12 +0100

    wip

--- scaffold.sh
+++ scaffold.sh
@@ -1,4 +1,4 @@
-cd golang
+cd xld-operator
 rm -rf *
 operator-sdk init --domain digital.ai --repo github.com/xebialabs/xl-deploy-kubernetes-operator
 operator-sdk create api --group xld --version v1alpha1 --kind DigitalaiDeploy --resource --controller
